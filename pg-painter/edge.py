5d0d4d9f633d23f198e3d52d0ae89f7994c7e8b1
Joe-noh <goflb.jh@gmail.com>
2017-06-11 21:37:17 +0900

    make edge_detenction method

--- pg-painter/edge.py
+++ pg-painter/edge.py
@@ -2,13 +2,14 @@ import cv2
 import resizer
 import painter
 
-img = cv2.imread("./images/p.jpg")
-img = resizer.tshirt(img)
+def edge_detection(input, output):
+    img = cv2.imread(input)
+    img = resizer.tshirt(img)
 
-edge_detected = cv2.Canny(img, 50, 100)
-cv2.imwrite("./test.png", edge_detected)
+    edge_detected = cv2.Canny(img, 50, 100)
+    cv2.imwrite(output, edge_detected)
 
-img = cv2.imread("./test.png")
-img = painter.transparentize(img)
+    img = cv2.imread(output)
+    img = painter.transparentize(img)
 
-cv2.imwrite("./test.png", img)
+    cv2.imwrite(output, img)
